3624659bb5d55e62e7c24cbed71959fc5ca11a32
Frances Reagan James <adtgwen@gmail.com>
2025-06-25 00:38:35 -0400

    added due date validation and priority validation in Taskutils

--- src/components/utils/taskUtils.jsx
+++ src/components/utils/taskUtils.jsx
@@ -15,8 +15,40 @@ export const formatDate = (date) => {
   }
   };
 
-}
+  // task validatin with error messages//
+  export const validateTask = (task) => {
+    const errors = {};
 
+    // title validation//
+  if (!task.title || !task.title.trim()) {
+    errors.title = "Title is required and cannot be empty";
+  } else if (task.title.trim().length < 3) {
+    errors.title = "Title must be at least 3 characters long";
+  } else if (task.title.trim().length > 100) {
+    errors.title = "Title must be less than 100 characters";
+
+    // due date validation//
+    if(!task.dueDate){
+      errors.dueDate = "Due date is required";
+    }else{
+      const today = new Date();
+      const dueDate = newDate(task.dueDate);
+      // reset time for comparsion//
+      
+      if (dueDate<today){
+        errors.dueDate = "Due date cannot be in the past";
+      }
+    }
+// priority validation//
+if(!task.priority ||!["low","medium","high"].includes(task.priority)){
+  errors.priority = "Please select a valid priority";
+}
+return errors;
+  };
+  
+if
+
+    }
 function taskUtils() {
   return (
     <>
