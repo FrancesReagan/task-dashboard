4fecdee0042469b31796e530dcb3860da390da25
Frances Reagan James <adtgwen@gmail.com>
2025-06-26 23:43:29 -0400

    fixed typos in taskform

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -262,45 +262,9 @@ const handleSubmit = async(e) => {
             )}
             </div>
           </form>
-          
+
         </div>
-      )
+      );
       }
-
-    }
-}
-
-//   function handleSubmit(e){
-//     e.preventDefault()
-//    onAddTask(newTask)
-//    setForm({title:"", priority: "medium"})
-
-//     const newTask = {
-//     id: Date.now().toString(),
-//     title:  form.title,
-//     dueDate:form.dueDate,
-//     status: form.status,
-//     priority: form.priority,
-//   }
-//   }
-
-  
-//   return (
-//     <>
-//     <h1>TaskForm</h1>
-//     <form onSubmit={handleSubmit}>
-//       <input type="text" name="title" value={form.title} onChange={handleChange} placeholder="Task Title"/>
-//       <input type="date" name="dueDate" value={form.dueDate} onChange={handleChange}/>
-//       <select name="priority" onChange={handleChange} value={form.priority}>
-//         <option value="low">Low</option> 
-//         <option value="medium">Medium</option>
-//         <option value="High">High</option>
-//       </select>
-//       <input type="checkbox" />
-//       <button type="submit">Add Task</button>
-//     </form>
-//     </>
-//   )
-// }
 
 export default TaskForm;
