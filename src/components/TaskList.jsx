300cc98eef07d292c8be180b84acbf8338b57bd4
Frances Reagan James <adtgwen@gmail.com>
2025-06-27 00:43:26 -0400

    fixed issues in tasklist---corrected useMemo and filtering and sorting tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,38 @@
+import { useMemo } from "react";
 import TaskItem from "./TaskItem";
-function TaskList({tasks, onDelete}) {
-   console.log(tasks);
+import { filterTasks, sortTasks } from "../utils/taskUtils";
 
 
-  return (
-    <>
-    <h1>TaskList</h1>
-    <TaskItem key={tasks.id} task={task} onDelete={onDelete} /> </>
-    </TaskItem>
-  )
+
+function TaskList({
+  tasks,
+  filters,
+  sortBy,
+  onDelete,
+  onToggleCompleted,
+  onEdit,
+  onReorder,
+  theme,
+}) {
+
+  // memorized, filtered, and sorted tasks//
+  const filteredAndSortedTasks = useMemo(() => {
+    const filtered = filterTasks(tasks,filters);
+    return sortTasks(filtered,sortBy);
+  },[tasks,filters,sortBy]);
+
+  // reordering using handleDragEnd//
+  const handleDragEnd = (result) => {
+    if(!result.destination) return;
+
+    const items = Array.from(filteredAndSortedTasks);
+    const [reorderedItem] = items.splice(result.source.index,1);
+    items.splice(result.destination.index,0,reorderedItem); 
+
+    // pass reordered filtered tasks back to parent//
+    onReorder(items);
+  };
+
 }
 
 export default TaskList
